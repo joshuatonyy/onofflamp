cbf68e76b5367cb90518583999563c7cd5b06438
Joshua <joshuatony7@gmail.com>
2022-08-30 18:58:47 +0700

    main switch added

--- func.js
+++ func.js
@@ -1,6 +1,6 @@
 function saklar(lampno) {
-    if (lampno == 1) {
-        if (document.getElementById("toggle1").checked) {
+    if (lampno == 1 || 5) {
+        if (document.getElementById("toggle1").checked || document.getElementById("toggle5").checked) {
             console.log('on');
             document.getElementById("lampu1").src = 'assets/images/on.gif'
         }
@@ -9,8 +9,8 @@ function saklar(lampno) {
             document.getElementById("lampu1").src = 'assets/images/off.gif'
         }
     }
-    if (lampno == 2) {
-        if (document.getElementById("toggle2").checked) {
+    if (lampno == 2 || 5) {
+        if (document.getElementById("toggle2").checked || document.getElementById("toggle5").checked) {
             console.log('on');
             document.getElementById("lampu2").src = 'assets/images/on.gif'
         }
@@ -19,8 +19,8 @@ function saklar(lampno) {
             document.getElementById("lampu2").src = 'assets/images/off.gif'
         }
     }
-    if (lampno == 3) {
-        if (document.getElementById("toggle3").checked) {
+    if (lampno == 3 || 5) {
+        if (document.getElementById("toggle3").checked || document.getElementById("toggle5").checked) {
             console.log('on');
             document.getElementById("lampu3").src = 'assets/images/on.gif'
         }
@@ -29,5 +29,14 @@ function saklar(lampno) {
             document.getElementById("lampu3").src = 'assets/images/off.gif'
         }
     }
-
+    if (lampno == 4 || 5) {
+        if (document.getElementById("toggle4").checked || document.getElementById("toggle5").checked) {
+            console.log('on');
+            document.getElementById("lampu4").src = 'assets/images/on.gif'
+        }
+        else {
+            console.log('off');
+            document.getElementById("lampu4").src = 'assets/images/off.gif'
+        }
+    }
 }
